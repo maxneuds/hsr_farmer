6d6a7f582f05be527d57288ef94167d838e5e278
Maximilian Neudert <max@neudert.me>
2024-12-17 07:33:12 +0000

    minor bugfixes

--- src/worlds/penacony/penacony.py
+++ src/worlds/penacony/penacony.py
@@ -54,14 +54,14 @@ class Penacony:
         await self.audition_venue.farm()                        # TP:+0->3 R2:2
         await self.extra.restore_tp(tp=2, info='End of Penacony')
     async def dev(self):
-        # await self.dreams_edge.farm()                           # TP:-4->1 XP:7668/9612 Time:431    
-        # await self.base_zone.restore_tp(tp=4.1)                 # TP:+4->5 Time:???
-        # await self.childs_dream.farm()                          # TP:-3->2 R2:4 XP:5832/5832 Time:???
-        # await self.base_zone.restore_tp(tp=4.2)                 # TP:+4->5 Time:???
-        # await self.clock_studios_theme_park.farm()              # TP:-4->1 XP:7648/7648 Time:???
-        # await self.storage_zone.restore_tp(tp=4)                # TP:+4->5 Time:???
-        # await self.the_reverie_dreamscape.farm()                # TP:-4->1 XP:14580/15552 Time:? R2/4:1/2
-        # await self.golden_hour.restore_tp(tp=4.1)               # TP:+4->5 Time:???
+        await self.dreams_edge.farm()                           # TP:-4->1 XP:7668/9612 Time:431    
+        await self.base_zone.restore_tp(tp=4.1)                 # TP:+4->5 Time:???
+        await self.childs_dream.farm()                          # TP:-3->2 R2:4 XP:5832/5832 Time:???
+        await self.base_zone.restore_tp(tp=4.2)                 # TP:+4->5 Time:???
+        await self.clock_studios_theme_park.farm()              # TP:-4->1 XP:7648/7648 Time:???
+        await self.storage_zone.restore_tp(tp=4)                # TP:+4->5 Time:???
+        await self.the_reverie_dreamscape.farm()                # TP:-4->1 XP:14580/15552 Time:? R2/4:1/2
+        await self.golden_hour.restore_tp(tp=4.1)               # TP:+4->5 Time:???
         await self.dewlight_pavilion.farm()                     # TP:-14->1 XP:11448/11448 Time: R2/4:1/1
         await self.golden_hour.restore_tp(tp=4.2)               # TP:+4->5 Time:80
         await self.penacony_grand_theater.farm()                # TP:-2->3
